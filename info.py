86f7665f40e9d720b120db89109734ef5fd9d516
davidrlhilljr-cmyk <davidrlhilljr@gmail.com>
2025-09-25 22:48:30 -0400

    Add files via upload

---
+++ info.py
@@ -0,0 +1,77 @@
+
+#This File will contain the information to be displayed in your portfolio
+
+#CHANGE BELOW
+profile_picture = "Images/gojo.jpg"
+about_me = "I'm Satoru Gojo (五条 悟). My occupation is being a Special Grade Jujutsu Sorcerer and a teacher at Tokyo Jujutsu High. I'm also a huge fan of deserts "
+
+
+#CHANGE BELOW (OPTIONAL)
+linkedin_image_url = "https://content.linkedin.com/content/dam/me/business/en-us/amp/brand-site/v2/bg/LI-Bug.svg.original.svg"
+github_image_url = "https://cdn-icons-png.flaticon.com/256/25/25231.png"
+email_image_url = "https://logowik.com/content/uploads/images/513_email.jpg"
+
+#CHANGE BELOW
+my_linkedin_url = "https://www.linkedin.com/in/gojo-satoru-31a12a274/"
+my_github_url = "https://github.com/GooSatoru"
+my_email_address = "gojoSat@gmail.com"
+
+
+education_data ={
+    'Degree': 'Bachelor of Jujutsu in Mastery of Cursed Energy',
+    'Institution': 'Tokyo Metropolitan Curse Technical College',
+    'Location': 'Outskirts of Tokyo',
+    'Graduation Date': 'March 2010',
+    'GPA': '4.0'
+}
+course_data = {
+    "code":["CS 1301", "Math 6455", "Phys 2211", "Math 4431"], 
+    "names":["Intro to CS", "Differential Geometry I", "Principles of Physics I", "Introduction to Topology"], 
+    "semester_taken":["2nd", "3rd", "2nd", "2nd"],
+    "skills":["Python is not my favorite snake anymore", "The calculations are a pain.", "I'm full of potential after everything I've learned.", "I'm now so flexible in my thinking."],
+    }
+experience_data = {
+    "Tokyo Jujutsu High instructor": (["- Train the next generation of jujutsu sorcerers",
+                                                                          "- Mentor Itadori Yuji", "- Mentor Yuta Okkotsu"],"Images/teacher.webp"),
+    "Special Grade Sorcerer":(["- Confront and battle multiple special-grade curses",
+                                                           "- High-Level Mission Handling"],"Images/special.webp"),
+    
+
+}
+
+projects_data = {"Escorting the Star Plasma Vessel": (['Along with my best friend Suguru Geto, we were tasked with escorting and protecting a girl named Riko Amanai. Her planned fusion with the sorcerer Master Tengen is a critical mission for the jujutsu world.'], "Images/trio.webp"),}
+
+programming_data = {
+    'Cursed Energy Mastery': 90,
+    'Control of Domain Expansion': 100,
+    'Martial Arts Master': 80,
+    'Control of Limitless Technique': 90,
+    'Control of Six eyes': 90
+}
+
+#CHANGE BELOW (OPTIONAL)
+programming_icons = {
+    "Cursed Energy Mastery": "🌀",
+    "Control of Domain Expansion": "🤞",
+    "Martial Arts Master": "🥋",
+    "Control of Limitless Technique": "🫸🔵🔴🫷🫴🟣",
+    "Control of Six eyes": "♾️👁",
+}
+spoken_icons = {"English": "🇬🇧",
+    "Japanese": "🇯🇵",
+}
+
+#CHANGE BELOW
+spoken_data = {
+    "English": "Fluent",
+    "Japanese": "Fluent",
+}
+leadership_data = {
+    "Head of the Gojo Clan": (["- Used my position to champion a progressive agenda, embracing anyone with potential regardless of their background. "],"Images/ceremonial.webp"),
+    "Mentor and Trainer at Jujutsu High": (["- It is my duty to mentor and protect the next generation of jujutsu sorcerers. "],"Images/teacher2.jpg"),
+
+}
+activity_data={
+    "Gachinko Fight Club (Led by Kinji Hakari)": (["- A venue for hand-to-hand combat between sorcerers", 
+            "- Sometimes I would come down here to hone my martial art skills."], "Images/hakari.png"),
+}
