67b43a453907ac1d82569f8c782b5a7cd38b6fd8
davidrlhilljr-cmyk <davidrlhilljr@gmail.com>
2025-09-25 22:48:12 -0400

    Add files via upload

---
+++ Home_Page.py
@@ -0,0 +1,28 @@
+import streamlit as st
+
+# Title of App
+st.title("Web Development Lab01")
+
+# Assignment Data 
+# TODO: Fill out your team number, section, and team members
+
+st.header("CS 1301(Interactive Web Applications) - Section A - Made by David Hill")
+st.subheader("Web Development - Section X")
+st.subheader("Satoru Gojo")
+
+
+# Introduction
+# TODO: Write a quick description for all of your pages in this lab below, in the form:
+#       1. **Page Name**: Description
+#       2. **Page Name**: Description
+#       3. **Page Name**: Description
+#       4. **Page Name**: Description
+
+st.write("""
+Welcome to our Streamlit Web Development Lab01 app! You can navigate between the pages using the sidebar to the left. The following pages are:
+
+1. Gojo's Portfolio: A portflio page that showcases my many hobbies and skills as well as lists my education
+2. Quiz: Which Jujutsu Kaisen Character will you get?
+
+""")
+
